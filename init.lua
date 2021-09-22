42ee5b60dd494131ccb090e560a33d58941ae8b0
matthyno <34276633+matthyno@users.noreply.github.com>
2021-09-22 18:04:39 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
-io.write("Hello, World!")
+-- This allows writing component.modem.open(123) instead of writing
+-- component.getPrimary("modem").open(123), which may be nicer to read.
+setmetatable(component, { __index = function(_, key)
+                                      return component.getPrimary(key)
+                                    end})
+
+component.gpu.set(cursorX, cursorY, line)
 while(true)
 do
 end
